b5dcbce6f765b295453721f42900114dae47ca30
Gabriel <gambachidze99@gmail.com>
2025-02-06 17:36:02 +0400

    Fixed Spotify redirect URI

--- src/hooks/useSpotify.js
+++ src/hooks/useSpotify.js
@@ -6,18 +6,20 @@ const useSpotify = () => {
 
   useEffect(() => {
     const tokenFromUrl = queryString.parse(window.location.hash).access_token;
-    
+
     if (tokenFromUrl) {
       localStorage.setItem("spotify_token", tokenFromUrl);
       setToken(tokenFromUrl);
 
-      window.history.pushState("", "", window.location.pathname + window.location.search);
+      // Remove token from URL for security
+      window.history.replaceState({}, document.title, "/");
     } else {
       const savedToken = localStorage.getItem("spotify_token");
       if (savedToken) {
         setToken(savedToken);
       } else {
-        window.location.href = `https://accounts.spotify.com/authorize?response_type=token&client_id=${process.env.REACT_APP_SPOTIFY_CLIENT_ID}&redirect_uri=${window.location.origin}&scope=user-top-read`;
+        const redirectUri = "https://gaabbriell.github.io/final-react-spotify";
+        window.location.href = `https://accounts.spotify.com/authorize?response_type=token&client_id=${process.env.REACT_APP_SPOTIFY_CLIENT_ID}&redirect_uri=${redirectUri}&scope=user-top-read`;
       }
     }
   }, []);
