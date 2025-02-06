e8ef486287d834c2b52795d5a453ae8af57b2e8e
Gabriel <gambachidze99@gmail.com>
2025-02-06 19:04:39 +0400

    Fix OAuth redirect URI for Vercel

--- src/hooks/useSpotify.js
+++ src/hooks/useSpotify.js
@@ -17,7 +17,7 @@ const useSpotify = () => {
       if (savedToken) {
         setToken(savedToken);
       } else{
-        const redirectUri = "https://final-react-spotify.vercel.app/";
+        const redirectUri = process.env.REACT_APP_SPOTIFY_REDIRECT_URI;
         window.location.href = `https://accounts.spotify.com/authorize?response_type=token&client_id=${process.env.REACT_APP_SPOTIFY_CLIENT_ID}&redirect_uri=${redirectUri}&scope=user-top-read`;
       }
     }
